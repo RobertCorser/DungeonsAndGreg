8c724eb84abfcc927e06a2c83692f04d14168b4d
Bobby Corser <bobby.corser@gmail.com>
2019-02-15 12:57:54 -0500

    New project generated

---
+++ android/src/game/greg/dungeons/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package game.greg.dungeons;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import game.greg.dungeons.DungeonsAndGreg;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DungeonsAndGreg(), config);
+	}
+}
